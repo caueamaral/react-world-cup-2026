1376bbd95b730d97afa710069ca4019707380422
Cauê Amaral <caue.amaral@hotmail.com>
2026-05-18 09:53:38 -0300

    wip: Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+        <header>
+            <img
+                src="//cdn.worldvectorlogo.com/logos/mundial-2026-world-cup.svg"
+                alt="Fida World Cup 2026 - United States, Canada and Mexico"
+                style={{ height: '250px' }}
+            />
+        </header>
+    )
+}
